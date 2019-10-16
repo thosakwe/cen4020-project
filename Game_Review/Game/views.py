c5cb92d796425021b5a93d64de575225f837bb2e
Tobe O <thosakwe@gmail.com>
2019-10-16 12:56:17 -0400

    Basic New Game form

--- Game_Review/Game/views.py
+++ Game_Review/Game/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# TODO: Only allow admins to create games
+# TODO: *Should* only admins be allowed to create games?
+@login_required
+def new(request):
+  return render(request, 'Game/new_game.html')
