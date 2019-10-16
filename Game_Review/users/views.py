2bcd2422133752fe137671a514aafdbd92f00107
Tobe O <thosakwe@gmail.com>
2019-10-16 12:24:12 -0400

    Allow viewing other profiles besides your own

--- Game_Review/users/views.py
+++ Game_Review/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import EditProfileForm, UserRegistrationForm
+from django.contrib.auth.models import User
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from .models import Profile
@@ -31,33 +32,42 @@ def register(request):
 @login_required
 def profile(request):
     # Fetch the user's profile.
-    profile, _ = Profile.objects.get_or_create(user=request.user)
     errors = []
+    user_id = int(request.GET.get('user_id', request.user.id))
+    # TODO: If the user does not exist, display a 404 page.
+    the_user = User.objects.get(pk=user_id)
+    profile, _ = Profile.objects.get_or_create(user=the_user)
 
     if request.method == "POST":
-        form = EditProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            avatar = form.cleaned_data['avatar']
-            bio = form.cleaned_data['bio']
-
-            # If the user uploaded an avatar, save it.
-            # Validate image sizes, mime types, etc.
-            if avatar:
-                if avatar.content_type != "image/jpeg":
-                    errors.extend(["Profile pictures must be jPEG images."])
-                else:
-                    file_path = "review/static/avatars/" + str(request.user.id) + ".jpg"
-                    avatar_dir = os.path.dirname(file_path)
-                    if not os.path.exists(avatar_dir):
-                        os.makedirs(avatar_dir)
-                    with open(file_path, "wb") as file:
-                        for chunk in avatar.chunks():
-                            file.write(chunk)
-
-            # Update the user's profile.
-            profile.avatar_path = "/static/avatars/" + str(request.user.id) + ".jpg"
-            profile.bio = bio or profile.bio
-            profile.save()
+        if the_user.id != request.user.id:
+            errors.extend(["You can only edit your own profile."])
         else:
-            errors = form.errors
-    return render(request, 'users/profile.html', {'errors': errors, 'profile': profile})
+            form = EditProfileForm(request.POST, request.FILES)
+            if form.is_valid():
+                avatar = form.cleaned_data['avatar']
+                bio = form.cleaned_data['bio']
+
+                # If the user uploaded an avatar, save it.
+                # Validate image sizes, mime types, etc.
+                if avatar:
+                    if avatar.content_type != "image/jpeg":
+                        errors.extend(
+                            ["Profile pictures must be jPEG images."])
+                    else:
+                        file_path = "review/static/avatars/" + \
+                            str(the_user.id) + ".jpg"
+                        avatar_dir = os.path.dirname(file_path)
+                        if not os.path.exists(avatar_dir):
+                            os.makedirs(avatar_dir)
+                        with open(file_path, "wb") as file:
+                            for chunk in avatar.chunks():
+                                file.write(chunk)
+
+                # Update the user's profile.
+                profile.avatar_path = "/static/avatars/" + \
+                    str(the_user.id) + ".jpg"
+                profile.bio = bio or profile.bio
+                profile.save()
+            else:
+                errors = form.errors
+    return render(request, 'users/profile.html', {'errors': errors, 'profile': profile, 'the_user': the_user})
