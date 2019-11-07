6d1860718712f5185f257190597a3fe476de3b88
vdtstorm <51832333+vdtstorm@users.noreply.github.com>
2019-11-07 01:49:10 -0500

    Added display_video class to views

--- Game_Review/playthroughs/views.py
+++ Game_Review/playthroughs/views.py
@@ -66,3 +66,18 @@ class PlaythroughDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
             return True
         else:
             return False
+        
+def display_video(request):
+    lastvideo = Video.objects.last()
+
+    videofile = lastvideo.videofile
+
+    form = VideoForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+
+    context = {'videofile': videofile,
+               'form': form
+               }
+
+    return render(request, 'Blog/videos.html', context)
