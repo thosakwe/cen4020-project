3a2bed32b1499f342b9b02505adca8b164fcd63d
vdtstorm <51832333+vdtstorm@users.noreply.github.com>
2019-11-11 18:39:23 -0500

    added video uploads to playthrough model

--- Game_Review/playthroughs/admin.py
+++ Game_Review/playthroughs/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import playthroughs
-from .models import Video
+
 
 # Register your models here.
 # -*- coding: utf-8 -*-
 admin.site.register(playthroughs)
-admin.site.register(Video)
+
 
--- Game_Review/playthroughs/views.py
+++ Game_Review/playthroughs/views.py
@@ -4,7 +4,7 @@ from game.models import Game
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-from .models import Video
+#from .models import Video
 from .forms import VideoForm
 
 
@@ -15,7 +15,7 @@ from .models import playthroughs
 # Create your views here.
 def home(request):
     context = {
-        'Playthroughs': Playthroughs.objects.all()
+        'Playthroughs': playthroughs.objects.all()
     }
     return render(request, 'Playthroughs/home.html', context)
 def about(request):
@@ -71,7 +71,7 @@ class PlaythroughDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
             return False
         
 def display_video(request):
-    lastvideo = Video.objects.last()
+    lastvideo = playthroughs.objects.last()
 
     videofile = lastvideo.videofile
 
