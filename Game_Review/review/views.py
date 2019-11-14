37b2973bd02fe525e866dc2be44a5aa145643e6d
tajali016 <tta16@my.fsu.edu>
2019-11-14 09:55:59 -0500

    Fixed form resubmission issue on like/dislike/cpmment

--- Game_Review/review/views.py
+++ Game_Review/review/views.py
@@ -77,8 +77,7 @@ def review_detail(request, pk):
                 user_vote.delete()
             if user_vote.filter(vote=1).exists():
                 user_vote.get(vote=1).delete()
-        
-
+        return HttpResponseRedirect("")
     context = {
             'review': review,
             'comments': comments,
