57f2f8519eab1337038b4f04f0f16566716cd90b
Tobe O <thosakwe@gmail.com>
2019-12-04 11:55:09 -0500

    Use like instead of startswith for search

--- Game_Review/search/views.py
+++ Game_Review/search/views.py
@@ -6,14 +6,14 @@ from django.db.models import Q
 from django.contrib.postgres.search import SearchQuery
 
 
-def search(request): 
-        lst = []
-        name = ""
-        found = True
-        if request.method == 'POST':
-            form = SearchForm(request.POST)
-            if form.is_valid():
-                name = form.cleaned_data['name']
-                lst = Game.objects.filter(title__startswith=name)
-        searched = SearchForm()        
-        return render(request, 'search/search.html', {'searched': searched, 'result' : lst, 'found' : found })
+def search(request):
+    lst = []
+    name = ""
+    found = True
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            lst = Game.objects.filter(title__icontains=name)
+    searched = SearchForm()
+    return render(request, 'search/search.html', {'method': request.method, 'searched': searched, 'result': lst, 'found': found})
